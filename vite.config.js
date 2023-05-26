661e02fcaad58ba88be4bdd1c395175403a64316
Safwan <serajumsafwan25@gmail.com>
2023-05-26 17:00:59 -0300

    server tests ran

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import compression from 'vite-plugin-compression';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(()=> {
@@ -13,6 +15,10 @@ export default defineConfig(()=> {
       port: 3000,
     },
   
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(),
+    compression({
+    algorithm: 'Brotli', // gzip, brotliCompress, deflate are supported
+    ext: '.gz' // this is optional, '.gz' is default
+  })],
   };
 });
