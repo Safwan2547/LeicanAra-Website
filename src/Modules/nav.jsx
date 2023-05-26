7372bf564cda5d49bab98ed9d5c4078ca03c9bb1
Safwan <serajumsafwan25@gmail.com>
2023-05-26 17:19:26 -0300

    Mobile Nav bar X added

--- src/Modules/nav.jsx
+++ src/Modules/nav.jsx
@@ -15,7 +15,12 @@ const Navbar = () => {
         flex align-middle justify-between cursor-none fixed top-5 left-5 right-5 sm:top-8 sm:mr-5 sm:ml-5`} >
             <Link to="/" className=' buttonC font-Satoshi text-xl sm:text-3xl font-medium cursor-none'>LeicanAra</Link>
             <div className="links flex justify-between items-center">
-            <Link to="/DropDown" className=' sm:hidden bg-none border-solid border-2 border-black opacity-75 text-white font-bold w-6 h-6 p-1 rounded-full'>
+            <Link to="/DropDown" className=' opacity-80 sm:hidden bg-none  text-white font-bold w-6 h-6 p-1 rounded-full relative'>
+            
+            <div class="absolute inset-x-0 top-1/2 bg-black h-0.5 w-5 transform -rotate-90"></div>
+  <div class="absolute inset-x-0 top-1/2 bg-black h-0.5 w-5 transform"></div>
+
+            
                 </Link>
                 <Link to="/ContactPage" className=' hidden lg:block buttonC font-Satoshi text-2xl font-medium cursor-none'>Contact</Link>
                 {/* <Link to="/StarFall" className='buttonC font-Satoshi text-2xl font-medium cursor-none'>StarFall</Link> */}
