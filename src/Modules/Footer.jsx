d006d6c66bd4def5489e7c79a887aaec1284771c
Safwan <serajumsafwan25@gmail.com>
2024-01-04 18:47:55 +0600

    Footer update 2

--- src/Modules/Footer.jsx
+++ src/Modules/Footer.jsx
@@ -5,19 +5,19 @@ import instagram_logo from '../Assets/Instagram_Glyph_Black.svg';
 function Footer() {
   return (
     <footer className="relative p-10 sm:p-10 flex justify-normal items-center  bg-NightFall text-nightfall py-24">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-center items-center">
         {/* Braided star image */}
-        <div className="flex space-x-2 sm:justify-evenly sm:w-1/3">
+        {/* <div className="flex space-x-2 sm:justify-evenly sm:w-1/3">
           <img className='w-16 fill-white sm:w-16' src={logo} alt="LeicanAra logo" />
-        </div>
+        </div> */}
         
         {/* Text section */}
-        <div className='text-MainBeige text-center sm:text-xl align-start items-center justify-start hidden sm:flex'>
-          <p>Designed and Developed by LeicanAra<br />All Rights Reserved</p>
+        <div className='text-MainBeige  sm:text-center sm:text-sm   items-center justify-start text-left sm:flex'>
+          <p className='w-2/3 sm:w-full' >Designed and Developed by LeicanAra<br />All Rights Reserved</p>
         </div>
         
         {/* Footer icons */}
-        <div className="  w-1/3 flex justify-center flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-12">
+        <div className="  w-1/3 flex justify-center absolute left-2/3 flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-12">
           <a className="cursor-none  hover:scale-110 transition-all duration-300 buttonC" target="_blank" href='https://instagram.com/leicanara?igshid=MzMyNGUyNmU2YQ%3D%3D&utm_source=qr'>
           <svg className="buttonC" id="Layer_1" width="36" height="36" fill="#ffffff" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 533.33 533.33">
   <path class="cls-1" d="m266.67,0c-72.42,0-81.5.31-109.95,1.6-28.38,1.29-47.77,5.8-64.73,12.39-17.54,6.81-32.41,15.93-47.23,30.76-14.83,14.82-23.94,29.7-30.76,47.23-6.59,16.96-11.1,36.35-12.4,64.73-1.3,28.44-1.6,37.52-1.6,109.95s.31,81.5,1.6,109.95c1.3,28.38,5.8,47.77,12.4,64.73,6.81,17.54,15.93,32.41,30.76,47.23,14.83,14.82,29.7,23.94,47.23,30.76,16.96,6.59,36.35,11.1,64.73,12.39,28.44,1.3,37.52,1.6,109.95,1.6s81.5-.31,109.95-1.6c28.38-1.29,47.77-5.8,64.73-12.39,17.54-6.81,32.41-15.93,47.23-30.76,14.82-14.83,23.94-29.7,30.76-47.23,6.59-16.96,11.1-36.35,12.39-64.73,1.3-28.44,1.6-37.52,1.6-109.95s-.31-81.5-1.6-109.95c-1.29-28.38-5.8-47.77-12.39-64.73-6.81-17.54-15.93-32.41-30.76-47.23-14.83-14.82-29.7-23.94-47.23-30.76-16.96-6.59-36.35-11.1-64.73-12.39-28.44-1.3-37.52-1.6-109.95-1.6Zm0,48.05c71.2,0,79.64.27,107.76,1.56,26,1.19,40.12,5.53,49.52,9.18,12.45,4.84,21.33,10.62,30.66,19.95,9.33,9.33,15.11,18.22,19.95,30.66,3.65,9.4,8,23.52,9.18,49.52,1.28,28.12,1.56,36.55,1.56,107.76s-.27,79.64-1.56,107.76c-1.19,26-5.53,40.12-9.18,49.52-4.84,12.45-10.62,21.33-19.95,30.66-9.33,9.33-18.21,15.11-30.66,19.95-9.4,3.65-23.52,8-49.52,9.18-28.11,1.28-36.55,1.56-107.76,1.56s-79.64-.27-107.76-1.56c-26-1.18-40.12-5.53-49.52-9.18-12.45-4.84-21.33-10.62-30.66-19.95-9.33-9.33-15.11-18.21-19.95-30.66-3.65-9.4-8-23.52-9.18-49.52-1.28-28.12-1.56-36.55-1.56-107.76s.27-79.64,1.56-107.76c1.19-26,5.53-40.12,9.18-49.52,4.84-12.45,10.62-21.33,19.95-30.66,9.33-9.33,18.22-15.11,30.66-19.95,9.4-3.65,23.52-8,49.52-9.18,28.12-1.28,36.55-1.56,107.76-1.56"/>
